2c657709b6834f54830b51f62d2eeddf805d39f8
Lino <gustavoaraujoln728@gmail.com>
2025-01-28 01:09:29 -0300

    Esqueleto, estrutura de pastas e adição de alguns aneis

---
+++ js/produtos.js
@@ -0,0 +1,114 @@
+const Produto = [
+    
+    //Aneis
+    
+    {
+        id: 1,
+        nome: "Anel Solitário",
+        tipo: "anel",
+        preco: "10,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 2,
+        nome: "Anel de Onda",
+        tipo: "anel",
+        preco: "15,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 3,
+        nome: "Anel de Folha",
+        tipo: "anel",
+        preco: "15,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 4,
+        nome: "Anel de Coração (Vazado)",
+        tipo: "anel",
+        preco: "15,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 5,
+        nome: "Anel de Multiplos Corações",
+        tipo: "anel",
+        preco: "15,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 6,
+        nome: "Anel de Estrela",
+        tipo: "anel",
+        preco: "15,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 7,
+        nome: "Anel de Coração Maciço",
+        tipo: "anel",
+        preco: "20,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+    {
+        id: 8,
+        nome: "Anel Maciço",
+        tipo: "anel",
+        preco: "20,00R$",
+        tamanhos: "",
+        imagem: "../", 
+
+    },
+
+    // Braceletes
+    {
+        id: 100,
+        nome: "Bracelete",
+        tipo: "bracelete",
+        preco: "",
+    },
+    // Brincos
+    {
+        id: 200,
+        nome: "Brinco",
+        tipo: "brinco",
+        preco: "",
+    },
+    // Colares
+    {
+        id: 300,
+        nome: "Colar ",
+        tipo: "colar",
+        preco: "15,00R$",
+
+    },
+    //Pulseiras
+    {
+        id: 400,
+        nome: "Pulseira",
+        tipo: "pulseira",
+        preco: "",
+    },
+    // Tornozeleiras
+    {
+        id: 500,
+        nome: "Pulseira",
+        tipo: "pulseira",
+        preco: "",
+    }
+]
