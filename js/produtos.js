d1485ca03f88ba6c4532f2fa163f8674a7a655f8
Lino <gustavoaraujoln728@gmail.com>
2025-01-29 21:17:21 -0300

    Estilo funcionando nos itens, botões estilizados e funcionando

--- js/produtos.js
+++ js/produtos.js
@@ -1,77 +1,76 @@
 const Produto = [
-    
     //Aneis
     
     {
         id: 1,
         nome: "Anel Solitário",
         tipo: "anel",
-        preco: "10,00R$",
+        preco: "10,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 2,
         nome: "Anel de Onda",
         tipo: "anel",
-        preco: "15,00R$",
+        preco: "15,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 3,
         nome: "Anel de Folha",
         tipo: "anel",
-        preco: "15,00R$",
+        preco: "15,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 4,
         nome: "Anel de Coração (Vazado)",
         tipo: "anel",
-        preco: "15,00R$",
+        preco: "15,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 5,
         nome: "Anel de Multiplos Corações",
         tipo: "anel",
-        preco: "15,00R$",
+        preco: "15,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 6,
         nome: "Anel de Estrela",
         tipo: "anel",
-        preco: "15,00R$",
+        preco: "15,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 7,
         nome: "Anel de Coração Maciço",
         tipo: "anel",
-        preco: "20,00R$",
+        preco: "20,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
     {
         id: 8,
         nome: "Anel Maciço",
         tipo: "anel",
-        preco: "20,00R$",
+        preco: "20,00",
         tamanhos: "",
-        imagem: "../", 
+        imagem: "./img/", 
 
     },
 
@@ -94,7 +93,7 @@ const Produto = [
         id: 300,
         nome: "Colar ",
         tipo: "colar",
-        preco: "15,00R$",
+        preco: "15,00",
 
     },
     //Pulseiras
@@ -107,8 +106,10 @@ const Produto = [
     // Tornozeleiras
     {
         id: 500,
-        nome: "Pulseira",
-        tipo: "pulseira",
-        preco: "",
+        nome: "Tornozeleira de Buzios",
+        tipo: "tornozeleira",
+        preco: "15",
     }
 ]
+
+export default Produto;
