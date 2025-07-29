8f5b48303256f93da0e7bcebbb797935c8a60054
GustavoLino <gustavoaraujoln728@gmail.com>
2025-07-29 00:44:50 -0300

    Feat: admin screen configured, produts routes, persisting data and storing images in a supabase bucket

---
+++ backend/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from routes.products import products_bp
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Permite chamadas do frontend
+
+app.register_blueprint(products_bp)
+
+if __name__ == '__main__':
+    app.run(debug=True)
