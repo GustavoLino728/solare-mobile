546ff8522b7ce75c41eda86ac93608c6aa27a877
Gustavo Lino <gustavo.lino@arpe.local>
2025-07-31 11:52:29 -0300

    Fix: fixing another CORS issue

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+app.config.update(
+    SESSION_COOKIE_SAMESITE="None",  
+    SESSION_COOKIE_SECURE=True    
+)
 app.secret_key = os.getenv("SECRET_KEY")
 CORS(app,origins=["http://127.0.0.1:5500", "http://localhost:5500", "https://solare-catalogo.vercel.app"],supports_credentials=True) 
 
