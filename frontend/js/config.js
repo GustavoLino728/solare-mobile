d8649b5c002cf7fce35b174ee89ca1c5ebef3dcd
GustavoLino <gustavoaraujoln728@gmail.com>
2025-08-14 18:37:25 -0300

    Fix: Change in the admin section to correlate tag to products

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,4 @@
-export const API_URL = "http://127.0.0.1:5000/api"
-// const dev = window.location.hostname === 'localhost';
-// export const API_URL = dev
-//   ? 'http://localhost:5000/api'
-//   : 'https://solare-catalogo-hukk.onrender.com/api';
+const dev = window.location.hostname === 'localhost';
+export const API_URL = dev
+  ? 'http://localhost:5000/api'
+  : 'https://solare-catalogo-hukk.onrender.com/api';
